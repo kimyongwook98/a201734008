17afd7980bf4df7f9136ed92c7ad94c45fbf5756
Kim, yongwook <kyw4846@gmail.com>
2023-09-20 10:31:20 +0900

    five

---
+++ src/a201734008/Five.java
@@ -0,0 +1,7 @@
+package a201734008;
+
+public class Five {
+	public static void print() {
+		System.out.println("five");
+	}
+}
--- src/a201734008/Main.java
+++ src/a201734008/Main.java
@@ -2,6 +2,7 @@ package a201734008;
 
 public class Main {
 	public static void main(String[] args) {
+		Five.print();
 		Two.print();
 		System.out.println("one 201734008");
 		Three.print();
